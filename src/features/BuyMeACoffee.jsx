fdc42bf144d63935f9d7548fd3a29ce33d24c0b4
SOMWHY <1928135011@qq.com>
2025-01-04 21:34:26 +0800

    add qr code

--- src/features/BuyMeACoffee.jsx
+++ src/features/BuyMeACoffee.jsx
@@ -6,8 +6,12 @@ import { Link } from 'react-router-dom'
 export default function BuyMeACoffee() {
   return (
     <div 
+    style={{ 
+      background:"url(`/assets/coffee-bg.jpg`)",
+      backgroundSize: "contain",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",}}
     
-    data-bg="/assets/coffee-bg.jpg"
      className="lazy-background relative flex justify-center items-center p-4 w-screen h-screen "  >
      <div className="absolute inset-0 dark:bg-malibu-950/60 bg-malibu-800/60"></div>
      <div className=' z-50'>
--- src/main.jsx
+++ src/main.jsx
@@ -16,18 +16,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
-document.addEventListener("DOMContentLoaded", function() {
-  const lazyBackgrounds = document.querySelectorAll(".lazy-background");
-  
-  const imageObserver = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const img = entry.target;
-        img.style.backgroundImage = `url(${img.dataset.bg})`;
-        observer.unobserve(img);
-      }
-    });
-  });
-  
-  lazyBackgrounds.forEach(img => imageObserver.observe(img));
-});
